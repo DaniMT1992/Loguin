b805788966f219888f1913a5e9f34df1ef0072b5
Daniel Mosquer Taborda <danielmosquera@technium.dev>
2024-09-17 14:38:38 -0500

    Add project files.

---
+++ Loguin/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Loguin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+    }
+}
